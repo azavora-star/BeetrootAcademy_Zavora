c807ac9c18a175d9f121fecd73e244df2962ce08
azavora <azavora@gmail.com>
2023-06-11 18:05:31 +0100

    interim work changes

--- basics.py
+++ basics.py
@@ -1,10 +1,16 @@
 #Lesson:"Перші Кроки" 
 
 '''Task 1
+
 Run the python interpreter via the terminal. 
-Get familiar with running python commands in the terminal, work with output, etc.
+Get familiar with running python commands in the terminal, work with output, etc.'''
+
+print('Done')
  
-Task 2
+###########################################################################################################################################################################
+
+'''Task 2
+
 Create a python program named "task2", and use the built-in function 'print' in it several times. 
 Try to pass "sep", "end" params and pass several parameters separated by commas. 
 Also, provide a comment text above each print statement, mentioned above, 
@@ -13,11 +19,31 @@ with the expected output after execution of the particular print statement.
 (Ex.
 # 'hello world'
 print("hello world")
-)
+)'''
+
+######### task2 programm ########
+
+msg1 = 'У царя Трояна цапині вуха'
+msg2 = 'Точно, я атвічаю'
+
+# Case 1: appying 'end' parameter
+# Expected Outcome: 'У царя Трояна цапині вуха?'
+print(msg1, end = '?')
+
+# Case 2: appying 'sep' parameter
+# Expected Outcome: 'У царя Трояна цапині вуха?!! Точно, я атвічаю'
+print(msg1, msg2, sep= ' ?!! ')
+
+# Case 3: appying both 'sep' and 'end' parameters
+# Expected Outcome: 'У царя Трояна цапині вуха?!! Точно, я атвічаю'
+print(msg1, msg2, end= ', бля!', sep= '?!! ')
+
+#############################################################################################################################################################################
  
-Task 3
+'''Task 3
 Write a program, which has two print statements to print the following text 
 (capital letters "O" and "H", made from "#" symbols):
+
 #########
 #       #
 #       #
@@ -33,5 +59,3 @@ Write a program, which has two print statements to print the following text
 Pay attention that usage of spaces is forbidden, as well as creating the whole result 
 text string using """ """, use '\n' and '\t' symbols instead.
 '''
-
-print('hello VSC')
