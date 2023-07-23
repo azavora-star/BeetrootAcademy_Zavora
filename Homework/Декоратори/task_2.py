219ded44afb8e112cb50abb2d323ce56df81e4c9
azavora <azavora@gmail.com>
2023-07-23 21:26:28 +0100

    Homework Декоратори -1,2

---
+++ Homework/Декоратори/task_2.py
@@ -0,0 +1,17 @@
+# Write a decorator that takes a list of stop words and replaces them with * inside the decorated function
+
+def stop_words(words: list):
+    def decorator(func):
+        def wrapper(name):
+            str = func(name)
+            for word in words:
+                str = str.replace(word, '*')
+            return str
+        return wrapper
+    return decorator
+
+@stop_words(['pepsi', 'BMW'])
+def create_slogan(name: str) -> str:
+    return f"{name} drinks pepsi in his brand new BMW!"
+
+assert create_slogan("Steve") == "Steve drinks * in his brand new *!"
