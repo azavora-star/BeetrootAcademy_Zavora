f06eefefd16d6662c8b1b2cbba60acef071b89bd
azavora <azavora@gmail.com>
2023-07-25 22:12:25 +0100

    task 3 done

--- Homework/Класи, область видимості та простір імен/task_3.py
+++ Homework/Класи, область видимості та простір імен/task_3.py
@@ -8,7 +8,8 @@
 #     next_channel() - turns on the next channel. If the current channel is the last one, turns on the first channel.
 #     previous_channel() - turns on the previous channel. If the current channel is the first one, turns on the last channel.
 #     current_channel() - returns the name of the current channel.
-#     is_exist(N/'name') - gets 1 argument - the number N or the string 'name' and returns "Yes", if the channel N or 'name' exists in the list, or "No" - in the other case.
+#     is_exist(N/'name') - gets 1 argument - the number N or the string 'name' and returns "Yes", if the channel N or 'name' exists in the list, 
+#     or "No" - in the other case.
 
 # The default channel turned on before all commands is №1.
 
@@ -31,30 +32,49 @@ class TVController:
     def turn_channel(self, n):
         self.n = n
         print(self.CHANNELS[self.n - 1])
-        TVController.n = self.n
+        TVController.n = self.n - 1
 
     def next_channel(self):
+        if TVController.n >= len(CHANNELS):
+            TVController.n = 1
+            print(self.CHANNELS[TVController.n])
+        else:
+            print(self.CHANNELS[TVController.n + 1])
+            TVController.n += 1
+
+    def previous_channel(self):
+        if TVController.n == -len(CHANNELS):
+            TVController.n = len(CHANNELS) - 1
+            print(self.CHANNELS[TVController.n - 1])
+        else:
+            print(self.CHANNELS[TVController.n - 1])
+            TVController.n -= 1  
+
+    def current_channel(self):
         print(self.CHANNELS[TVController.n])
 
-
-
-
+    def is_exist(self, argument):
+        if isinstance(argument, str) is True:
+            if argument in self.CHANNELS:
+                print('Yes')
+            else:
+                print('No')
+        else:
+            try:
+                self.CHANNELS[argument]
+                print('Yes')
+            except IndexError:
+                print('No')                     
 
 
 
 controller = TVController(CHANNELS)
 
-
-controller.first_channel()
-controller.last_channel()
-controller.turn_channel(1)
-controller.next_channel() 
-print(TVController.n)
-
-# controller.previous_channel() == "BBC"
-
-# controller.current_channel() == "BBC"
-
-# controller.is_exist(4) == "No"
-
-# controller.is_exist("BBC") == "Yes"
+controller.first_channel() == "BBC"
+controller.last_channel() == "TV1000"
+controller.turn_channel(1) == "BBC"
+controller.next_channel() == "Discovery"
+controller.previous_channel() == "BBC"
+controller.current_channel() == "BBC"
+controller.is_exist(4) == "No"
+controller.is_exist("BBC") == "Yes"
