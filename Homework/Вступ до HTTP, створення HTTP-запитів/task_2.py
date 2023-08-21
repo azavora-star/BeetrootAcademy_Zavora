9edfdc53fb1c0a0420d14fb7832d8c4e6b95c025
azavora <azavora@gmail.com>
2023-08-21 11:46:21 +0100

    вступ до http homewor tasks 1-3

--- Homework/Вступ до HTTP, створення HTTP-запитів/task_2.py
+++ Homework/Вступ до HTTP, створення HTTP-запитів/task_2.py
@@ -8,3 +8,6 @@ import requests
 comments = requests.get('https://api.pushshift.io/reddit/comment/search/')
 print(comments.status_code, comments.reason)
 print(comments.json())
+
+# seems the api doesn't work anymore and server returns code 403 "FORBIDDEN"
+# skipping this task
